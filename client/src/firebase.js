ba3a52ba79384950ed2ad64990ed1d3378841f50
Nahid nawal nayim <e191013@ugrad.iiuc.ac.bd>
2024-07-06 21:04:48 +0600

    initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWSIZdMKZchBmLd72SPpKE5Ie6qnlEkXY",
+  authDomain: "real-auth-a87b5.firebaseapp.com",
+  projectId: "real-auth-a87b5",
+  storageBucket: "real-auth-a87b5.appspot.com",
+  messagingSenderId: "853544353845",
+  appId: "1:853544353845:web:95fd9030877e89282fb17c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
